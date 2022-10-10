ad12a75f39e099e005723e8194ba7ae03e26d922
Mila <lakralima@mail.ru>
2022-10-10 23:54:05 +0300

    Full task is ready

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // либо задать на старте выполнения алгоритма.
 
 Console.WriteLine("Эта программа формирует из введённого массива строк новый массив, длина которого <= 3 символа.");
-Console.WriteLine("Сколько элементов будет в первоначальном массиве? NB! Вводить их придётся самостоятельно:)");
+Console.WriteLine("Сколько строк будет в первоначальном массиве? NB! Вводить их придётся вам:)");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[size];
 
@@ -23,3 +23,21 @@ void PrintArray(string[] array)
 }
 Console.Write("Ваш массив: ");
 PrintArray(array);
+
+string[] secondArray = new string [array.Length];
+
+void ArrayWithThreeSights(string[] array, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            secondArray[count] = array[i];
+            count++;
+        }
+    }
+}
+ArrayWithThreeSights(array, secondArray);
+Console.Write("Трансформированный массив: ");
+PrintArray(secondArray);
